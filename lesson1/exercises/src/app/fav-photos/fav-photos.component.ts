e1e30e128bb66f3a6bc723337d713f3e89fdd379
Natalie Sutton <100653980+nataliersutton@users.noreply.github.com>
2022-08-22 17:40:21 -0500

    Updated movie list, chores list, and fav photos as directed in exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Always More Cat Pictures!';
+  image1 = 'https://i.pinimg.com/originals/cb/5e/af/cb5eafb205bc2a1914fe240e5d441cf9.jpg';
+  image2 = 'https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1920%2C1920&ssl=1';
+  image3 = 'https://thechive.com/wp-content/uploads/2022/07/c7e73b75f7c73a7b1f923c5f6185de34_width-600-1.jpg?attachment_cache_bust=4146554&quality=85&strip=info&w=292&h=231&crop=1';
 
   constructor() { }
 
