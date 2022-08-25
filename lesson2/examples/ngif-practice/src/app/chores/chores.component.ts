7bbfdd43daecd02ecd4542927152ba385ac7f5f6
Natalie Sutton <100653980+nataliersutton@users.noreply.github.com>
2022-08-25 18:16:09 -0500

    Completed examples and exercises for lesson 2

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Empty dishwasher'];
+   finishedChores = ["Mail package", 'Complete LaunchCode prep work', "Walk dog", "Do laundry",  "Prep dinner"];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://us.123rf.com/450wm/juliarstudio/juliarstudio1602/juliarstudio160202817/51977844-target-with-dart-flat-icon-isolated-on-white-background.jpg?ver=6';
 
    constructor() { }
 
